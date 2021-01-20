032e536e6c6ce61eb00f0d9aaa9265ebb5f9533c
Chase Wickett <wickettc@yahoo.com>
2021-01-19 22:19:03 -0500

    added logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "../css/Home.css";
+import cwLogo from "../imgs/cw-logo.png";
 
 const Home = () => {
     return (
         <div className="home-container">
-            <h2 className="home-first">Hey there,</h2>
-            <h2 className="home-second">
-                I'm <span className="my-name">Chase Wickett</span>
-            </h2>
-            <h2 className="home-third">Web Developer</h2>
+            <div className="home-title">
+                <h2 className="home-first">Hey there,</h2>
+                <h2 className="home-second">
+                    I'm <span className="my-name">Chase Wickett</span>
+                </h2>
+                <h2 className="home-third">Web Developer</h2>
+            </div>
+            <div className="home-logo-container">
+                <img
+                    className="my-logo"
+                    src={cwLogo}
+                    alt="Chase Wickett WebDev Logo"
+                />
+            </div>
         </div>
     );
 };
