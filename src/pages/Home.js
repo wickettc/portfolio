53ce3c6ef62f6934f76395802ef68a08a436e886
Chase Wickett <wickettc@yahoo.com>
2021-01-19 19:34:40 -0500

    adjusted background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import '../css/Home.css';
+import React from "react";
+import "../css/Home.css";
 
 const Home = () => {
     return (
         <div className="home-container">
             <h2 className="home-first">Hey there,</h2>
-            <h1 className="home-second">I'm Chase Wickett</h1>
+            <h2 className="home-second">
+                I'm <span className="my-name">Chase Wickett</span>
+            </h2>
             <h2 className="home-third">Web Developer</h2>
         </div>
     );
