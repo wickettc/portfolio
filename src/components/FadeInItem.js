8d30be90edd257434341dba2f2f98d088ab2e86b
Chase Wickett <wickettc@yahoo.com>
2021-02-16 17:20:27 -0500

    added appear on scroll functionality

---
+++ src/components/FadeInItem.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const FadeInItem = (props) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const itemRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!isVisible) setIsVisible(entry.isIntersecting);
+            });
+        });
+        observer.observe(itemRef.current);
+        const cleanupItem = itemRef.current;
+        return () => observer.unobserve(cleanupItem);
+    }, [isVisible]);
+    return (
+        <div
+            style={{ margin: '100px' }}
+            className={`fade-in-item ${isVisible ? 'is-visible' : ''}`}
+            ref={itemRef}
+        >
+            {props.children}
+        </div>
+    );
+};
+
+export default FadeInItem;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,92 @@
 import React from 'react';
 import '../css/Home.css';
+import FadeInItem from '../components/FadeInItem';
 import cwLogo from '../imgs/cw-logo.png';
 import chaseDevLogo from '../imgs/chase_dev_logo.png';
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <div className="home-title">
-                {/* <h2 className="home-first">Hey there,</h2>
-                <h2 className="home-second">
-                    I'm <span className="my-name">Chase Wickett</span>
-                </h2>
-                <h2 className="home-third">Web Developer</h2> */}
-                <h1>
-                    <span className="home-break home-first">Hey there,</span>
-                    <span className="home-break home-second">
-                        I'm <span className="my-name">Chase Wickett</span>
-                    </span>
-                    <span className="home-break home-third">Web Developer</span>
-                </h1>
+        <div>
+            <div className="home-container">
+                <div className="home-title">
+                    <h1>
+                        <span className="home-break home-first">
+                            Hey there,
+                        </span>
+                        <span className="home-break home-second">
+                            I'm <span className="my-name">Chase</span>
+                        </span>
+                        <span className="home-break home-third">
+                            Web Developer
+                        </span>
+                    </h1>
+                </div>
+                <div className="home-logo-container">
+                    <img
+                        className="my-logo"
+                        width="80%"
+                        src={chaseDevLogo}
+                        alt="Chase Wickett WebDev Logo"
+                    />
+                </div>
             </div>
-            <div className="home-logo-container">
-                <img
-                    className="my-logo"
-                    width="80%"
-                    src={chaseDevLogo}
-                    alt="Chase Wickett WebDev Logo"
-                />
+            <div className="home-about-container">
+                <FadeInItem>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent fermentum sapien quis massa placerat, in
+                        dignissim mi scelerisque. Sed cursus tortor in volutpat
+                        euismod. Quisque laoreet molestie mauris. Aenean porta
+                        nisi ac pulvinar dignissim. Curabitur sed vulputate
+                        nisi, in porta mauris. Donec varius dui vitae vestibulum
+                    </div>
+                    <hr></hr>
+                </FadeInItem>
+
+                <FadeInItem>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent fermentum sapien quis massa placerat, in
+                        dignissim mi scelerisque. Sed cursus tortor in volutpat
+                        euismod. Quisque laoreet molestie mauris. Aenean porta
+                        nisi ac pulvinar dignissim. Curabitur sed vulputate
+                        nisi, in porta mauris. Donec varius dui vitae vestibulum
+                    </div>
+                    <hr></hr>
+                </FadeInItem>
+                <FadeInItem>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent fermentum sapien quis massa placerat, in
+                        dignissim mi scelerisque. Sed cursus tortor in volutpat
+                        euismod. Quisque laoreet molestie mauris. Aenean porta
+                        nisi ac pulvinar dignissim. Curabitur sed vulputate
+                        nisi, in porta mauris. Donec varius dui vitae vestibulum
+                    </div>
+                    <hr></hr>
+                </FadeInItem>
+                <FadeInItem>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent fermentum sapien quis massa placerat, in
+                        dignissim mi scelerisque. Sed cursus tortor in volutpat
+                        euismod. Quisque laoreet molestie mauris. Aenean porta
+                        nisi ac pulvinar dignissim. Curabitur sed vulputate
+                        nisi, in porta mauris. Donec varius dui vitae vestibulum
+                    </div>
+                    <hr></hr>
+                </FadeInItem>
+                <FadeInItem>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent fermentum sapien quis massa placerat, in
+                        dignissim mi scelerisque. Sed cursus tortor in volutpat
+                        euismod. Quisque laoreet molestie mauris. Aenean porta
+                        nisi ac pulvinar dignissim. Curabitur sed vulputate
+                        nisi, in porta mauris. Donec varius dui vitae vestibulum
+                    </div>
+                    <hr></hr>
+                </FadeInItem>
             </div>
         </div>
     );
